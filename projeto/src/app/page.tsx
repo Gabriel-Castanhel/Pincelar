37e91c0203cca94f9544d4d34421846a7e8d9a68
gabriel-castanhel <gabrielcastanhel2@gmail.com>
2026-02-18 19:59:08 -0300

    style, colocando e melhorando a estilização do meu site

--- projeto/src/app/page.tsx
+++ projeto/src/app/page.tsx
@@ -1,15 +1,58 @@
 import Header from "@/components/header";
 import Hero_banner from "@/components/Hero_banner";
 
+// Icones
+import { FaPalette } from "react-icons/fa";
+import { GiReceiveMoney } from "react-icons/gi";
+import { FaHandsHelping } from "react-icons/fa";
+
+
 export default function Home() {
   return (
-    <div className="bg-gray-900 text-white min-h-screen p-4">
+    <div className="text-black min-h-screen p-4">
       <div>
         <Header />
       </div>
       <div>
         <Hero_banner />
       </div>
+
+      <section className="flex flex-col items-center justify-center gap-8 h-150">
+        <h1 className="text-4xl font-bold text-center mt-8 text-gradient-primary font-banner">
+          Por que escolher o Pincelar?
+        </h1>
+        <div className="flex gap-4 w-2/3">
+          <div className="bg-color-2 w-full max-w-md flex flex-col gap-4 text-center">
+            <FaPalette className="text-blue-500 text-5xl hover:text-red-500 transition-colors flex justify-center items-center" />
+
+            <h2 className="text-2xl font-bold mb-4">Variedade de Produtos</h2>
+            <p>
+              Cada peça é criada por artistas da comunidade com paixão e
+              dedicação.
+            </p>
+          </div>
+          <div className="bg-color-2 w-full max-w-md flex flex-col gap-4 text-center">
+            <GiReceiveMoney className="text-blue-500 text-5xl hover:text-red-500 transition-colors flex justify-center items-center" />
+
+            <h2 className="text-2xl font-bold mb-4">Qualidade Garantida</h2>
+            <p className="text-center">
+              Suporte direto aos artistas com preços transparentes e sem
+              intermediários.
+            </p>
+          </div>
+          <div className="bg-color-2 w-full max-w-md flex flex-col gap-4 text-center">
+            <FaHandsHelping className="text-blue-500 text-5xl hover:text-red-500 transition-colors flex justify-center items-center" />
+
+            <h2 className="text-2xl font-bold mb-4">
+              Atendimento Personalizado
+            </h2>
+            <p>
+              Nossa equipe está sempre pronta para ajudar, garantindo uma
+              experiência de compra tranquila e satisfatória.
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
--- projeto/src/components/Hero_banner/index.tsx
+++ projeto/src/components/Hero_banner/index.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 export default function index() {
     return (
-        <div className="bg-gradient-to-br from-[#F26E50] to-[#550F8F] text-white h-225 p-4 flex flex-col items-center justify-center gap-6">
+        <div className="bg-gradient-to-br from-[#F26E10] to-[#550F8F] text-white h-225 p-4 flex flex-col items-center justify-center gap-6">
             <div className='p-4 flex  items-center justify-center gap-20'>
-                <div className='w-150 whitespace-normal flex flex-col gap-4'>
-                    <h1 className="text-5xl font-banner text-center ">Descubra Arte Autêntica da Comunidade</h1>
+                <div className='w-150 whitespace-normal flex flex-col gap-6'>
+                    <h1 className="text-5xl font-banner ">Descubra Arte Autêntica da Comunidade</h1>
                     <p className='text-2xl font-banner'>Conectamos artistas talentosos com apaixonados por arte. Cada obra conta uma história única.</p>
                 </div>
                 <figure>
-                    <iframe src="https://www.youtube.com/embed/zQGQLEE1nQs?si=rG-iIf-bl132ZQgE" className='h-140 w-250 aspect-video rounded-xl' allow="autoplay; encrypted-media; control; picture-in-picture"></iframe>
+                    <iframe src="https://www.youtube.com/embed/zQGQLEE1nQs?si=rG-iIf-bl132ZQgE" className='h-140 w-250 aspect-video rounded-xl drop-shadow-xl/50' allow="autoplay; encrypted-media; control; picture-in-picture"></iframe>
                 </figure>
             </div>
         </div>
